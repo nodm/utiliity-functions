d107467261ad9fd9337f4eb445102190db9d16c1
Dmytro Novikov <dmytro.novikov@icloud.com>
2022-05-28 08:52:47 +0300

    Add extra parameter

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,3 +1,3 @@
-export default function getRandomElement(max = 10) {
-  return Math.round(Math.random() * max);
+export default function getRandomElement(min = 0, max = 10) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
